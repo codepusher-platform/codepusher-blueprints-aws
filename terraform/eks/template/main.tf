75a215e59bf03a9ca516e93743e128520d961ff1
Romulo Franca <romulo@codepusher.io>
2024-06-20 17:13:00 -0300

    fix(pre-commit): fix flag

--- terraform/eks/template/main.tf
+++ terraform/eks/template/main.tf
@@ -2,14 +2,14 @@ module "aws_eks" {
   source = "git::https://github.com/codepusher-platform/codepusher-blueprints-aws.git//terraform/eks/module?ref={{ .moduleVersion }}"
 
   cluster_name           = "{{ .clusterName }}"
-  instance_types         = "{{ .instanceTypes }}"
+  instance_types         = {{ .instanceTypes }}
   instance_class         = "{{ .instanceClass }}"
-  min_nodes              = "{{ .minNodes }}"
-  max_nodes              = "{{ .maxNodes }}"
-  desired_nodes          = "{{ .desiredNodes }}"
-  disk_capacity          = "{{ .diskCapacity }}"
+  min_nodes              = {{ .minNodes }}
+  max_nodes              = {{ .maxNodes }}
+  desired_nodes          = {{ .desiredNodes }}
+  disk_capacity          = {{ .diskCapacity }}
   owner                  = "{{ .owner }}"
   vpc_id                 = "{{ .vpcId }}"
-  vpc_private_subnet_ids = "{{ .vpcPrivateSubnetIds }}"
+  vpc_private_subnet_ids = {{ .vpcPrivateSubnetIds }}
   environment_name       = "{{ .environmentName }}"
 }
