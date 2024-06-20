15ab7bd44a8b8adaec52e6ff84053cc0d605b461
Romulo Franca <romulo@codepusher.io>
2024-06-20 17:19:17 -0300

    fix(eks): fix output

--- terraform/eks/template/outputs.tf
+++ terraform/eks/template/outputs.tf
@@ -1,14 +1,14 @@
 output "aws_eks_cluster_name" {
-  value       = module.aws_eks.aws_eks_cluster_name
+  value       = module.aws_eks.cluster_name
   description = "EKS Cluster Name"
 }
 
-output "cluster_endpoint" {
+output "aws_eks_cluster_endpoint" {
   value       = module.aws_eks.cluster_endpoint
   description = "EKS Cluster API Endpoint"
 }
 
 output "aws_eks_cluster_arn" {
-  value       = module.aws_eks.aws_eks_cluster_arn
+  value       = module.aws_eks.cluster_arn
   description = "EKS Cluster ARN"
 } 
