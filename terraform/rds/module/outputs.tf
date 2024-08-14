e6f9ef20832453f96796667bfd506a87bffc8657
Romulo Franca <71389109+romulofranca@users.noreply.github.com>
2024-08-14 16:47:40 -0300

    fix(module/rds): changed output values

--- terraform/rds/module/outputs.tf
+++ terraform/rds/module/outputs.tf
@@ -1,19 +1,19 @@
 output "cluster_endpoint" {
   description = "The endpoint for the primary instance of the RDS cluster."
-  value       = module.rds_cluster.cluster_endpoint
+  value       = module.rds_cluster.endpoint
 }
 
 output "cluster_reader_endpoint" {
   description = "The reader endpoint for the RDS cluster."
-  value       = module.rds_cluster.cluster_reader_endpoint
+  value       = module.rds_cluster.reader_endpoint
 }
 
 output "cluster_arn" {
   description = "The ARN of the RDS cluster."
-  value       = module.rds_cluster.cluster_arn
+  value       = module.rds_cluster.arn
 }
 
 output "cluster_id" {
   description = "The ID of the RDS cluster."
-  value       = module.rds_cluster.cluster_id
+  value       = module.rds_cluster.cluster_resource_id
 }
